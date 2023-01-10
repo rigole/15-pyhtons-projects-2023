7cb4c92f25b638068ac5517d5a5e777107f64e9e
rigole <foplacide@gmail.com>
2023-01-10 14:47:45 +0100

    first project URL shortener

---
+++ URL-Shortener/main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+window = Tk()
+
+window.title("Url Shortener")
+window.minsize(width=500, height=500)
+
+url_label = Label(text="Enter URL")
+url_label.grid(column=2, row=2, rowspan=4)
+
+
+
+
+
+
+
+
+
+window.mainloop()
