4e854f61e04c495d3c645c2839f3cff1aa497997
rigole <foplacide@gmail.com>
2023-01-11 11:27:12 +0100

    setting the final postion of each element

--- URL-Shortener/main.py
+++ URL-Shortener/main.py
@@ -2,16 +2,25 @@ from tkinter import *
 
 window = Tk()
 
+
+def shorten_function():
+    print("Hello World")
+
 window.title("Url Shortener")
 window.minsize(width=500, height=500)
 
+title_label = Label(text= "Welcome to the URL shortener")
+title_label.grid(column=0, row=2, rowspan=2, pady=25)
+title_label.config(font=("Courier", 35),justify="center")
+
 url_label = Label(text="Enter URL")
-url_label.grid(column=2, row=2, rowspan=4)
+url_label.grid(column=0, row=5, rowspan=1,pady=10)
 
+url_input = Entry(width=75)
+url_input.grid(column=0, row=6, rowspan=6, pady=10, ipady=3)
 
-
-
-
+button_converter = Button(text="Shorten URL", width=25, command=shorten_function)
+button_converter.grid(pady=10, column=0, row=18, rowspan=8)
 
 
 
