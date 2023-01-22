bcc608b2650f969a5d8e6a2b16e414ce6e4cd559
rigole <foplacide@gmail.com>
2023-01-22 13:33:50 +0100

    adding pyttsx3 for voice

---
+++ Desktop_Assistant/main.py
@@ -0,0 +1,8 @@
+import pyttsx3
+
+assistant = pyttsx3.init()
+
+
+assistant.say("Hello My name is placid. How can I help you")
+
+assistant.runAndWait()
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This is a repository I created for my Python projects in 2023 to increase my skills
 
 1) URL shortener
+2) Desktop Assistant
