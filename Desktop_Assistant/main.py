bfa1d527a92f4cea7f089cf503e38ecd2b5f29c5
rigole <foplacide@gmail.com>
2023-01-27 12:13:06 +0100

    adding and testing speechRecognition

--- Desktop_Assistant/main.py
+++ Desktop_Assistant/main.py
@@ -1,8 +1,30 @@
+import speech_recognition as speechRecognition
 import pyttsx3
 
-assistant = pyttsx3.init()
 
 
-assistant.say("Hello My name is placid. How can I help you")
 
-assistant.runAndWait()
+recognition = speechRecognition.Recognizer()
+
+#converting text to speech
+
+def TexttoSpeak(command):
+    assistant = pyttsx3.init()
+    assistant.say(command)
+    assistant.runAndWait()
+
+
+
+#Using the microphone as input source for audio
+
+with speechRecognition.Microphone() as source2:
+    recognition.adjust_for_ambient_noise(source2, duration=0.2)
+    print("Please say something")
+    
+    audio2 = recognition.listen(source2)
+    
+    text = recognition.recognize_google(audio2, language="en-US",show_all=True)
+
+    
+    print(text['alternative'])
+    TexttoSpeak(text)
